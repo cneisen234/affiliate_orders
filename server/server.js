ae1613e27914be70638929fcc027f68731924959
Christopher Neisen <christopherjay71186@gmail.com>
2020-11-18 19:10:46 -0600

    trying again

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("server/public"));
+app.use(express.static("build"));
 
 let config = {
   headers: {
@@ -18,16 +18,16 @@ let config = {
     "X-Auth-Token": "13n6uxj2je2wbnc0vggmz8sqjl93d1d",
   },
 };
-   if (process.env.NODE_ENV === "production") {
-     // Exprees will serve up production assets
-     app.use(express.static("client/build"));
+  //  if (process.env.NODE_ENV === "production") {
+  //    // Exprees will serve up production assets
+  //    app.use(express.static("client/build"));
 
-     // Express serve up index.html file if it doesn't recognize route
-     const path = require("path");
-     app.get("*", (req, res) => {
-       res.sendFile(path.resolve(__dirname, "public", "index.html"));
-     });
-   }
+  //    // Express serve up index.html file if it doesn't recognize route
+  //    const path = require("path");
+  //    app.get("*", (req, res) => {
+  //      res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  //    });
+  //  }
 const bot = new SlackBot({
   token: "xoxb-18234618080-1485620851799-hzRxtdbDZJSuh7w7wiTOQ4cF",
   name: "AffiliateOrders",
